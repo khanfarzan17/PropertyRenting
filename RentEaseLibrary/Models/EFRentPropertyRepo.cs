e8dd922b004e2e54b7024f38db7b219199f428cb
khanfarzan17 <khanfarzan200@gmail.com>
2024-01-13 19:42:41 +0530

    Model Of User and Rent Property is Being Complete

---
+++ RentEaseLibrary/Models/EFRentPropertyRepo.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentEaseLibrary.Models
+{
+    public class EFRentPropertyRepo : IRentPropertRepo
+    {
+
+       PropertyRentingDbContext ctx = new PropertyRentingDbContext();
+
+
+        public async Task DeleteRentProperty(int PropertyId)
+        {
+            RentProperty rentProperty= await GetPropertyById(PropertyId);
+            ctx.RentProperties.Remove(rentProperty);
+            ctx.SaveChanges();
+        }
+
+        public async Task<List<RentProperty>> GetAllProperts()
+        {
+            List<RentProperty> properties = await ctx.RentProperties.ToListAsync();
+            return properties;
+        }
+
+        public async Task<RentProperty> GetPropertyById(int PropertyId)
+        {
+            try
+            {
+                RentProperty properties = await (from p in ctx.RentProperties where p.PropertyId == PropertyId select p).FirstAsync();
+                return properties;
+
+            }
+            catch (Exception)
+            {
+                throw new Exception("No such Proprty Id is Found");
+            }
+        }
+
+        public async Task InsertRentProperty(RentProperty property)
+        {
+            await ctx.RentProperties.AddAsync(property);
+            await ctx.SaveChangesAsync();
+
+        }
+
+        public async Task UpdateRentProperty(int PropertyId, RentProperty property)
+        {
+            RentProperty property2update= await GetPropertyById(PropertyId);
+
+            property2update.PropertyName= property.PropertyName;
+            property2update.Location= property.Location;
+            property2update.Type= property.Type;
+            property2update.Price= property.Price;
+
+            await ctx.SaveChangesAsync();
+        }
+    }
+}
---
+++ RentEaseLibrary/Models/EFUserRepo.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentEaseLibrary.Models
+{
+    public class EFUserRepo : IUserRepo
+    {
+         PropertyRentingDbContext ctx= new PropertyRentingDbContext();
+        public async Task DeleteUserByUserName(string userName)
+        {
+            User user =await GetUserByUserName(userName);
+            ctx.Users.Remove(user);
+            await ctx.SaveChangesAsync();
+        }
+
+        public async Task<List<User>> GetAllUser()
+        {
+            List<User> users = await ctx.Users.ToListAsync();
+            return users;
+        }
+
+        public async Task<User> GetUserByUserName(string userName)
+        {
+            try
+            {
+                User user = await (from u in ctx.Users where u.UserName == userName select u).FirstAsync();
+                if(user == null)
+                {
+                    throw  new InvalidOperationException("No such UserName is found ");
+                    
+                }
+                return user;
+
+
+            }
+            catch (Exception ex )
+            {
+                throw new Exception("An error occurred while retrieving the user by UserName", ex);
+            }
+        }
+
+        public async Task InsertUser(User user)
+        {
+           await ctx.Users.AddAsync(user);
+            await ctx.SaveChangesAsync();
+        }
+
+        public Task Login(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
